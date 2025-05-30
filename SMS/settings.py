a42a07c0768b13c687ae0aa537985807af124968
maaaaateyo <mathewpaulino09.mp@gmail.com>
2025-05-30 20:40:23 +0800

    student template css

--- SMS/settings.py
+++ SMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure--@za=qlktztede^%r)k#g5h_0uyynmbf@d8agz4l-(y@1vo*!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
